86c3e2711b39bce88b2f801570a04513fa0fc635
Jona Hyun <jona.hyun@gmail.com>
2017-03-09 19:28:48 -0500

    video 4 & unable to load property listing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,56 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule, Routes } from '@angular/router';
+import { AngularFireModule, AuthProviders, AuthMethods } from 'angularfire2';
+import { FirebaseService } from './services/firebase.service';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { ListingsComponent } from './components/listings/listings.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { ListingComponent } from './components/listing/listing.component';
+import { AddListingComponent } from './components/add-listing/add-listing.component';
+import { EditListingComponent } from './components/edit-listing/edit-listing.component';
+
+export const firebaseConfig = {
+    apiKey: "AIzaSyBxCHy7iFa1noXMY1NCbZVJ-g_-Ncfos2k",
+    authDomain: "proplistings-a3d01.firebaseapp.com",
+    databaseURL: "https://proplistings-a3d01.firebaseio.com",
+    storageBucket: "proplistings-a3d01.appspot.com",
+    messagingSenderId: "449613986706"
+};
+
+const firebaseAuthConfig = {
+  provider: AuthProviders.Google,
+  method: AuthMethods.Popup
+};
+
+
+const appRoutes: Routes = [
+  {path: '', component:HomeComponent},
+  {path: 'listings', component:ListingsComponent},
+  {path: 'add-listing', component:AddListingComponent}
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ListingsComponent,
+    NavbarComponent,
+    ListingComponent,
+    AddListingComponent,
+    EditListingComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AngularFireModule.initializeApp(firebaseConfig, firebaseAuthConfig),
+    RouterModule.forRoot(appRoutes)
   ],
-  providers: [],
+  providers: [FirebaseService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/services/firebase.service.ts
+++ src/app/services/firebase.service.ts
@@ -4,16 +4,17 @@ import { AngularFire, FirebaseListObservable } from 'angularfire2';
 @Injectable()
 export class FirebaseService {
   listings: FirebaseListObservable<any[]>;
+  
   constructor(private af: AngularFire) { }
 
   getListings() {
-    this.listings = this.af.database.list('/listings') as FirebaseListObservable<Listing[]>
+    this.listings = this.af.database.list('/listings') as FirebaseListObservable<Listing[]>;
     return this.listings;
   }
 
 }
 
-interface Listing{
+interface Listing {
   $key?:string;
   title?:string;
   type?:string;
